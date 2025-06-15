5beaa5f21583c3865b973b63a1c909f05ddd5aee
0xF3546 <99101632+0xF3546@users.noreply.github.com>
2025-06-15 17:32:13 +0200

    Added Search Controller

---
+++ webapi/src/visiom.Api/Search/SearchController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace visiom.Api.Search
+{
+    [ApiController]
+    [Route("/api/[controller]")]
+    public class SearchController : ControllerBase
+    {
+
+        [HttpGet]
+        public IActionResult Search([FromQuery(Name = "q")] string query)
+        {
+            return Ok();
+        }
+    }
+}
