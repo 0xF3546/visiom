05c68bf941e14fd7acbbb22ea3af72116890cdae
0xF3546 <99101632+0xF3546@users.noreply.github.com>
2025-06-16 20:16:36 +0200

    Added OpenAPI Codegenerator

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,8 +1,12 @@
+import { QueryClientProvider } from '@tanstack/react-query';
 import AppRouter from './AppRouter';
+import { queryClient } from './utils/queryClien';
 
 function App() {
   return (
+    <QueryClientProvider client={queryClient}>
     <AppRouter />
+    </QueryClientProvider>
   );
 }
 
